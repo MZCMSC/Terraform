f004d6eb574d180b7c8fb5050842499a3e3516c2
MZCMSC <ygcho@mz.co.kr>
2022-10-17 17:24:24 +0900

    Modify Code S3/VPC

--- CODE/ver_4_hierarchy/00_S3/state-backend.tf
+++ CODE/ver_4_hierarchy/00_S3/state-backend.tf
@@ -1,17 +1,10 @@
 resource "aws_s3_bucket" "terraform_state_backend" {
   bucket = var.s3_bucket
-
-  # Terraform destroy시 에러 및 삭제 불가
-  # lifecycle {
-  #   prevent_destroy = true
-  # }
-
-  tags = merge(var.tags, tomap({ Name = format("%s", var.s3_bucket) }))
+  tags   = merge(var.tags, tomap({ Name = format("%s", var.s3_bucket) }))
 }
 resource "aws_s3_bucket_acl" "this" {
   bucket = aws_s3_bucket.terraform_state_backend.id
   acl    = var.s3_acl
-  # acl = "private"
 }
 
 resource "aws_s3_bucket_versioning" "this" {
@@ -24,7 +17,6 @@ resource "aws_s3_bucket_versioning" "this" {
 resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   bucket = aws_s3_bucket.terraform_state_backend.id
   rule {
-    # bucket_key_enabled = 
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
