adbf4cf2a79c272343d68bd7d04cff3fe75c3f96
MZCMSC <ygcho@mz.co.kr>
2022-10-19 16:49:03 +0900

    Update SG_forder tf file

--- CODE/ver_5_hierarchy/02_SG/security_group.tf
+++ CODE/ver_5_hierarchy/02_SG/security_group.tf
@@ -4,13 +4,11 @@ resource "aws_default_security_group" "this" {
 }
 
 resource "aws_security_group" "this" {
-    for_each = { for i in local.all_sg : i.name => i  }
+  for_each = { for i in local.all_sg : i.name => i }
 
-    name = format("%s-tf-msc-%s-security-groups", var.prefix, each.value.name)
-    
-    vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
-
-    tags = merge(var.tags, tomap({ Name = format("%s-tf-msc-%s-security-group", var.prefix, each.value.name)}))
+  name   = format("%s-tf-msc-%s-security-groups", var.prefix, each.value.name)
+  vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
+  tags   = merge(var.tags, tomap({ Name = format("%s-tf-msc-%s-security-group", var.prefix, each.value.name) }))
 }
 
 // for_each 문을 사용하여 생성한 값
