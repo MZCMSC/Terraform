13845e9419e0627879df517b4f5831b2b491cc5d
MZCMSC <ygcho@mz.co.kr>
2022-10-19 16:44:55 +0900

    Update file

--- CODE/ver_5_hierarchy/01_VPC/main.tf
+++ CODE/ver_5_hierarchy/01_VPC/main.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
 
-backend "s3" {
+  backend "s3" {
     bucket         = "test-terraform-state-backend-msc"
     dynamodb_table = "test-terraform-state-locks"
     key            = "vpc/terraform.tfstate"
@@ -15,3 +15,37 @@ backend "s3" {
     encrypt        = true
   }
 }
+
+
+
+
+# Module Block 설명할때 예제
+
+/*
+module "vpc" {
+  source = "../module/vpc/"
+
+  region                  = var.region
+  prefix                  = var.prefix
+  azs                     = var.azs
+  vpc_name                = var.vpc_name
+  vpc_cidr                = var.vpc_cidr
+  enable_dns_hostnames    = var.enable_dns_hostnames
+  enable_dns_support      = var.enable_dns_support
+  enable_internet_gateway = var.enable_internet_gateway
+  enable_nat_gateway      = var.enable_nat_gateway
+  subnets                 = var.subnets
+  tags                    = var.tags
+} */
+
+/* 
+module ->
+
+tfvars ->
+
+variable ->
+
+
+
+
+*/
